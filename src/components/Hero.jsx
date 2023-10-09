ce47d86d20f17e5cdf2bc617ea38462d9fee3daf
Safwan Hassan <safwan.hassan.d@gmail.com>
2023-10-09 13:25:51 +0500

    Create the responsive navbar component and the hero section

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,9 +1,49 @@
-import React from 'react'
+import React from "react";
+import { motion } from "framer-motion";
+import { styles } from "../styles";
+import { ComputersCanvas } from "./canvas";
 
 const Hero = () => {
   return (
-    <div>Hero</div>
-  )
-}
+    <section className='relative w-full h-screen mx-auto'>
+      <div
+        className={`${styles.paddingX} absolute inset-0 top-[120px] max-w-7xl mx-auto flex flex-row items-start gap-5`}
+      >
+        <div className='flex flex-col justify-center items-center mt-5'>
+          <div className='w-5 h-5 rounded-full bg-[#915eff]' />
+          <div className='w-1 sm:h-80 h-40 violet-gradient' />
+        </div>
+        <div>
+          <h1 className={`${styles.heroHeadText} text-white`}>
+            Hi, I'm <span className='text-[#915eff]'>Safwan</span>
+          </h1>
+          <p className={`${styles.heroSubText} mt-2 text-white-100`}>
+            I develop web applications and APIs, with a passion for website
+            design. {/*<br className="sm:block hidden" />*/} I'm all about
+            animations and 3D renderings on the web.
+          </p>
+        </div>
+      </div>
 
-export default Hero
+      <ComputersCanvas />
+
+      <div className='absolute xs:bottom-10 bottom-32 w-full flex justify-center items-center'>
+        <a href='#about'>
+          <div className='w-[25px] h-[44px] rounded-3xl border-4 border-secondary flex justify-center items-start py-2'>
+            <motion.div
+              animate={{ y: [0, 12, 0] }}
+              transition={{
+                duration: 1.5,
+                repeat: "infinite",
+                repeatType: "loop",
+              }}
+              className='w-1.5 h-1.5 rounded-full bg-secondary mb-1'
+            />
+          </div>
+        </a>
+      </div>
+    </section>
+  );
+};
+
+export default Hero;
